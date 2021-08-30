eb9efed3ff87fd13447af9578c12997356200651
lambtor <lambtor@yahoo.com>
2021-08-30 14:50:55 -0500

    Add files via upload

---
+++ android/support/v4/animation/AnimatorCompatHelper.java
@@ -0,0 +1,27 @@
+package android.support.v4.animation;
+
+import android.os.Build.VERSION;
+import android.support.annotation.RestrictTo;
+import android.view.View;
+
+@RestrictTo({RestrictTo.Scope.GROUP_ID})
+public final class AnimatorCompatHelper {
+   private static final AnimatorProvider IMPL;
+
+   static {
+      if (VERSION.SDK_INT >= 12) {
+         IMPL = new HoneycombMr1AnimatorCompatProvider();
+      } else {
+         IMPL = new GingerbreadAnimatorCompatProvider();
+      }
+
+   }
+
+   public static void clearInterpolator(View var0) {
+      IMPL.clearInterpolator(var0);
+   }
+
+   public static ValueAnimatorCompat emptyValueAnimator() {
+      return IMPL.emptyValueAnimator();
+   }
+}
