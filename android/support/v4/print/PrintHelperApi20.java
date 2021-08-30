dd2600e02811f971a4f2be40a6029ce4d6fc95b1
lambtor <lambtor@yahoo.com>
2021-08-30 15:51:49 -0500

    Add files via upload

---
+++ android/support/v4/print/PrintHelperApi20.java
@@ -0,0 +1,10 @@
+package android.support.v4.print;
+
+import android.content.Context;
+
+class PrintHelperApi20 extends PrintHelperKitkat {
+   PrintHelperApi20(Context var1) {
+      super(var1);
+      this.mPrintActivityRespectsOrientation = false;
+   }
+}
---
+++ android/support/v4/print/PrintHelperApi24.java
@@ -0,0 +1,11 @@
+package android.support.v4.print;
+
+import android.content.Context;
+
+class PrintHelperApi24 extends PrintHelperApi23 {
+   PrintHelperApi24(Context var1) {
+      super(var1);
+      this.mIsMinMarginsHandlingCorrect = true;
+      this.mPrintActivityRespectsOrientation = true;
+   }
+}
