d18697fd1d37549dcacbc81106335bc3dda0d1eb
lambtor <lambtor@yahoo.com>
2021-08-30 15:12:49 -0500

    Add files via upload

---
+++ android/support/v4/media/TransportStateListener.java
@@ -0,0 +1,9 @@
+package android.support.v4.media;
+
+public class TransportStateListener {
+   public void onPlayingChanged(TransportController var1) {
+   }
+
+   public void onTransportControlsChanged(TransportController var1) {
+   }
+}
